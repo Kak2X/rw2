19ffdda079e90dfdc8a285f6f9fc1cafebe55c08
Kak <kak32x@gmail.com>
2025-10-01 00:59:32 +0200

    Preserve registers before adding to the song queue
    Not doing so spectacularly broke the Get Weapon screen, always save them just to be sure

--- driver/interface.asm
+++ driver/interface.asm
@@ -1,7 +1,20 @@
 SECTION "Sound Driver Interface for the player", ROM0
 SoundInt_Init: mHomeCallRet Sound_StopAll_Main
 SoundInt_Do: mHomeCallRet Sound_Do_Main
-SoundInt_ReqPlayId:	mHomeCallRet Sound_ReqPlayId_Main
+SoundInt_ReqPlayId:	
+	push hl
+	push de
+		ldh  a, [hROMBank]
+		push af
+			ld   a, BANK(Sound_ReqPlayId_Main)
+			mBankswitch
+			call Sound_ReqPlayId_Main
+		pop  af
+		mBankswitch
+	pop  de
+	pop  hl
+	ret
+	
 SoundInt_SetVolume: mHomeCallRet Sound_SetVolume_Main
 SoundInt_StartSlide: mHomeCallRet Sound_StartSlide_Main
 Bankswitch:
